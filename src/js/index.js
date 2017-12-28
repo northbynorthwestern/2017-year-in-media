55e502499592e7dcf9a59cebab86776059769697
Maxine Whitely <m.whitely16@gmail.com>
2017-12-28 14:17:36 -0600

    load hbs templates foreach story in archie.json

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,12 @@
 var Handlebars = require("handlebars");
 var copy = require('../data/archie.json');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+var prettifyHtml = require('prettify-html');
+
+//Load Handlebars partials
+var storyWindowTemplate = require("./../templates/partials/event-window.hbs");
+
 
 // Set up DOM Elements--FROM OL SITE INDEX. use as a template for loading hbs templates
 // var upcomingEventsPanelDOM = document.querySelector('#upcoming-events-panel');
@@ -10,7 +17,7 @@ var copy = require('../data/archie.json');
 // var upcomingEventsPanelTemplate = require("./../templates/partials/events-list-small.hbs");
 // var eventsListFullTemplate = require("./../templates/partials/events-list-full.hbs");
 // var lightningTalksTemplate = require("./../templates/partials/lightning-talks-archive.hbs");
-var storyWindowTemplate = require("./../templates/partials/event-window.hbs");
+// var storyWindowTemplate = require("./../templates/partials/event-window.hbs");
 
 // Set up global variables
 // var moment = require('moment');
@@ -19,40 +26,73 @@ var storyWindowTemplate = require("./../templates/partials/event-window.hbs");
 // var staticCopy = require('json-loader!../data/static-copy.json');
 
 
+// ???
+/******* var source = document.getElementById("entry-template").innerHTML; *****/
+/******* jQuery version: var source = $("#some-template").html(); *****/
+/******* var template = Handlebars.compile(source); *****/
+// ???
+function runIt(copy, error){
+  var stories = copy.stories;
+  var storytemp = Handlebars.compile(storyWindowTemplate);
+  var dir = '../../out/';
+  mkdirp.sync(dir, function (err) {
+      if (err) console.error("err")
+      else console.log('pow!')
+  });
+  var fileName = '../../out/index.html';
+  var stream = fs.createWriteStream(fileName);
 
-var source = document.getElementById("entry-template").innerHTML;
-// jQuery version: var source = $("#some-template").html();
+  for (var x = 0; x < stories.length; ++x) {
+    var context = {
+      hed: stories[x]['hed'],
+      author: stories[x]['author'],
+      lead: stories[x]['lead'],
+      story: stories[x]['story']
+    }
 
-var template = Handlebars.compile(source);
+		var storywindow = prettifyHtml(storytemp(context));
+		// var storywin = prettifyHtml(storywindow);
+		stream.write(storywindow);
+		stream.end();
+    if (error){
+      console.log("error!!")
+    }
+  }
+}
+
+
 
 //////////////
 //registers handlebars partials -- FROM OL SITE
 
-var globby = require('globby'),
-    fs = require('fs-extra');
-var getPartial = function(Handlebars, rootPath) {
-  globby.sync(rootPath + '*.hbs').forEach(function(file) {
-    var partialName = file.replace(rootPath, "").replace(".hbs", "");
-    Handlebars.registerPartial(partialName, fs.readFileSync(file, "utf8"));
-  })
-}
-module.exports = {
-  getPartial: getPartial
-}
-
-// runs this function
-getPartial(Handlebars, './../templates/partials/');
-//////////////
-
-function onLoad(data, tabletop) {
-    copy = data;
-    copy.events = GroupEvents(processEventsSheet(data.events.elements));
-    updateDOM();
-}
 
 
-function updateDOM() {
-    eventsListFullDOM.innerHTML = eventsListFullTemplate(copy.events);
-}
 
-$('body').append(template(data)); //jQuery
+// var globby = require('globby'),
+//     fs = require('fs-extra');
+// var getPartial = function(Handlebars, rootPath) {
+//   globby.sync(rootPath + '*.hbs').forEach(function(file) {
+//     var partialName = file.replace(rootPath, "").replace(".hbs", "");
+//     Handlebars.registerPartial(partialName, fs.readFileSync(file, "utf8"));
+//   })
+// }
+// module.exports = {
+//   getPartial: getPartial
+// }
+//
+// // runs this function
+// getPartial(Handlebars, './../templates/partials/');
+// //////////////
+//
+// function onLoad(data, tabletop) {
+//     copy = data;
+//     copy.events = GroupEvents(processEventsSheet(data.events.elements));
+//     updateDOM();
+// }
+//
+//
+// function updateDOM() {
+//     eventsListFullDOM.innerHTML = eventsListFullTemplate(copy.events);
+// }
+//
+// $('body').append(template(data)); //jQuery
